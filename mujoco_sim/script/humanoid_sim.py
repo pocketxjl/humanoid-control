f818258e5382551fed1d9601b52b98277cb8de8a
jlxue <xuejl2001@mail.ustc.edu.cn>
2024-02-11 11:45:07 +0800

    add mujoco sim

---
+++ mujoco_sim/script/humanoid_sim.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+import mujoco as mj
+import numpy as np
+from mujoco_base import MuJoCoBase
+from mujoco.glfw import glfw
+import rospy
+import rospkg
+from std_msgs.msg import Float32MultiArray,Bool
+from nav_msgs.msg import Odometry
+
+
+class HectorSim(MuJoCoBase):
+  def __init__(self, xml_path):
+    super().__init__(xml_path)
+    self.simend = 1000.0
+    # print('Total number of DoFs in the model:', self.model.nv)
+    # print('Generalized positions:', self.data.qpos)  
+    # print('Generalized velocities:', self.data.qvel)
+    # print('Actuator forces:', self.data.qfrc_actuator)
+    # print('Actoator controls:', self.data.ctrl)
+    # mj.set_mjcb_control(self.controller)
+    # * Set subscriber and publisher
+
+    # initialize target joint position, velocity, and torque
+    self.targetPos = np.array([0, 0, 0.35, -0.90, -0.55, 0, 0, 0, 0.35, -0.90, -0.55, 0])
+    self.targetVel = np.zeros(12)
+    self.targetTorque = np.zeros(12)
+    self.targetKp = np.ones(12) * 30
+    self.targetKd = np.ones(12) * 2
+
+    self.pubJoints = rospy.Publisher('/jointsPosVel', Float32MultiArray, queue_size=10)
+    self.pubOdom = rospy.Publisher('/ground_truth/state', Odometry, queue_size=10)
+
+    rospy.Subscriber("/targetTorque", Float32MultiArray, self.targetTorqueCallback) 
+    rospy.Subscriber("/targetPos", Float32MultiArray, self.targetPosCallback) 
+    rospy.Subscriber("/targetVel", Float32MultiArray, self.targetVelCallback)
+    rospy.Subscriber("/targetKp", Float32MultiArray, self.targetKpCallback)
+    rospy.Subscriber("/targetKd", Float32MultiArray, self.targetKdCallback)
+    #set the initial joint position
+    self.data.qpos[:3] = np.array([0, 0, 0.98])
+    self.data.qpos[-12:] = np.array([0, 0, 0.35, -0.90, -0.55, 0, 0, 0, 0.35, -0.90, -0.55, 0])
+
+    # * show the model
+    mj.mj_step(self.model, self.data)
+    # enable contact force visualization
+    self.opt.flags[mj.mjtVisFlag.mjVIS_CONTACTFORCE] = True
+
+    # get framebuffer viewport
+    viewport_width, viewport_height = glfw.get_framebuffer_size(
+        self.window)
+    viewport = mj.MjrRect(0, 0, viewport_width, viewport_height)
+    # Update scene and render
+    mj.mjv_updateScene(self.model, self.data, self.opt, None, self.cam,
+                        mj.mjtCatBit.mjCAT_ALL.value, self.scene)
+    mj.mjr_render(viewport, self.scene, self.context)
+    
+
+  def targetTorqueCallback(self, data):
+    self.targetTorque = data.data
+
+  def targetPosCallback(self, data):
+    self.targetPos = data.data
+
+  def targetVelCallback(self, data):
+    self.targetVel = data.data 
+
+  def targetKpCallback(self, data):
+    self.targetKp = data.data
+
+  def targetKdCallback(self, data):
+    self.targetKd = data.data
+
+  def reset(self):
+    # Set camera configuration
+    self.cam.azimuth = 89.608063
+    self.cam.elevation = -11.588379
+    self.cam.distance = 5.0
+    self.cam.lookat = np.array([0.0, 0.0, 1.5])
+
+  # def controller(self, model, data):
+  #   self.data.ctrl[0] = 100
+  #   pass
+
+  def simulate(self):
+    while not glfw.window_should_close(self.window):
+      simstart = self.data.time
+
+      while (self.data.time - simstart <= 1.0/60.0 and not self.pause_flag):
+
+        # MIT control
+        self.data.ctrl[:] = self.targetTorque + self.targetKp * (self.targetPos - self.data.qpos[-12:]) + self.targetKd * (self.targetVel - self.data.qvel[-12:])
+        # Step simulation environment
+        mj.mj_step(self.model, self.data)
+        # * Publish joint positions and velocities
+        jointsPosVel = Float32MultiArray()
+        # get last 12 element of qpos and qvel
+        qp = self.data.qpos[-12:].copy()
+        qv = self.data.qvel[-12:].copy()
+        jointsPosVel.data = np.concatenate((qp,qv))
+
+        self.pubJoints.publish(jointsPosVel)
+        # * Publish body pose
+        bodyOdom = Odometry()
+        pos = self.data.sensor('BodyPos').data.copy()
+        ori = self.data.sensor('BodyQuat').data.copy()
+        vel = self.data.qvel[:3].copy()
+        angVel = self.data.sensor('BodyGyro').data.copy()
+        bodyOdom.header.stamp = rospy.Time.now()
+        bodyOdom.pose.pose.position.x = pos[0]
+        bodyOdom.pose.pose.position.y = pos[1]
+        bodyOdom.pose.pose.position.z = pos[2]
+        bodyOdom.pose.pose.orientation.x = ori[0]
+        bodyOdom.pose.pose.orientation.y = ori[1]
+        bodyOdom.pose.pose.orientation.z = ori[2]
+        bodyOdom.pose.pose.orientation.w = ori[3]
+        bodyOdom.twist.twist.linear.x = vel[0]
+        bodyOdom.twist.twist.linear.y = vel[1]
+        bodyOdom.twist.twist.linear.z = vel[2]
+        bodyOdom.twist.twist.angular.x = angVel[0]
+        bodyOdom.twist.twist.angular.y = angVel[1]
+        bodyOdom.twist.twist.angular.z = angVel[2]
+        self.pubOdom.publish(bodyOdom)
+
+      if self.data.time >= self.simend:
+          break
+
+      # get framebuffer viewport
+      viewport_width, viewport_height = glfw.get_framebuffer_size(
+          self.window)
+      viewport = mj.MjrRect(0, 0, viewport_width, viewport_height)
+
+      # Update scene and render
+      mj.mjv_updateScene(self.model, self.data, self.opt, None, self.cam,
+                          mj.mjtCatBit.mjCAT_ALL.value, self.scene)
+      mj.mjr_render(viewport, self.scene, self.context)
+
+      # swap OpenGL buffers (blocking call due to v-sync)
+      glfw.swap_buffers(self.window)
+
+      # process pending GUI events, call GLFW callbacks
+      glfw.poll_events()
+
+    glfw.terminate()
+
+def main():
+    # ros init
+    rospy.init_node('hector_sim', anonymous=True)
+
+    # get xml path
+    rospack = rospkg.RosPack()
+    rospack.list()
+    hector_desc_path = rospack.get_path('humanoid_legged_description')
+    xml_path = hector_desc_path + "/mjcf/humanoid_legged.xml"
+    sim = HectorSim(xml_path)
+    sim.reset()
+    sim.simulate()
+
+if __name__ == "__main__":
+    main()
---
+++ mujoco_sim/script/mujoco_base.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+import mujoco as mj
+from mujoco.glfw import glfw
+import rospy
+from std_msgs.msg import Bool 
+
+class MuJoCoBase():
+    def __init__(self, xml_path):
+        # For callback functions
+        self.button_left = False
+        self.button_middle = False
+        self.button_right = False
+        self.lastx = 0
+        self.lasty = 0
+        self.pause_flag = True
+        self.pubSimState = rospy.Publisher('/pauseFlag',Bool, queue_size=10)
+        # MuJoCo data structures
+        self.model = mj.MjModel.from_xml_path(xml_path)  # MuJoCo model
+        self.data = mj.MjData(self.model)                # MuJoCo data
+        self.cam = mj.MjvCamera()                        # Abstract camera
+        self.opt = mj.MjvOption()                        # visualization options
+
+        # Init GLFW, create window, make OpenGL context current, request v-sync
+        glfw.init()
+        self.window = glfw.create_window(1200, 900, "Demo", None, None)
+        glfw.make_context_current(self.window)
+        glfw.swap_interval(1)
+
+        # initialize visualization data structures
+        mj.mjv_defaultCamera(self.cam)
+        mj.mjv_defaultOption(self.opt)
+        self.scene = mj.MjvScene(self.model, maxgeom=10000)
+        self.context = mj.MjrContext(
+            self.model, mj.mjtFontScale.mjFONTSCALE_150.value)
+
+        # install GLFW mouse and keyboard callbacks
+        glfw.set_key_callback(self.window, self.keyboard)
+        glfw.set_cursor_pos_callback(self.window, self.mouse_move)
+        glfw.set_mouse_button_callback(self.window, self.mouse_button)
+        glfw.set_scroll_callback(self.window, self.scroll)
+
+    def keyboard(self, window, key, scancode, act, mods):
+        if act == glfw.PRESS and key == glfw.KEY_BACKSPACE:
+            mj.mj_resetData(self.model, self.data)
+            mj.mj_forward(self.model, self.data)
+        elif act ==glfw.PRESS and key== glfw.KEY_SPACE:
+            self.pause_flag = not self.pause_flag
+            mj.mj_forward(self.model, self.data)
+            simState = Bool()
+            simState.data = self.pause_flag
+            self.pubSimState.publish(simState)
+            
+
+    def mouse_button(self, window, button, act, mods):
+        # update button state
+        self.button_left = (glfw.get_mouse_button(
+            window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS)
+        self.button_middle = (glfw.get_mouse_button(
+            window, glfw.MOUSE_BUTTON_MIDDLE) == glfw.PRESS)
+        self.button_right = (glfw.get_mouse_button(
+            window, glfw.MOUSE_BUTTON_RIGHT) == glfw.PRESS)
+
+        # update mouse position
+        glfw.get_cursor_pos(window)
+
+    def mouse_move(self, window, xpos, ypos):
+        # compute mouse displacement, save
+        dx = xpos - self.lastx
+        dy = ypos - self.lasty
+        self.lastx = xpos
+        self.lasty = ypos
+
+        # no buttons down: nothing to do
+        if (not self.button_left) and (not self.button_middle) and (not self.button_right):
+            return
+
+        # get current window size
+        width, height = glfw.get_window_size(window)
+
+        # get shift key state
+        PRESS_LEFT_SHIFT = glfw.get_key(
+            window, glfw.KEY_LEFT_SHIFT) == glfw.PRESS
+        PRESS_RIGHT_SHIFT = glfw.get_key(
+            window, glfw.KEY_RIGHT_SHIFT) == glfw.PRESS
+        mod_shift = (PRESS_LEFT_SHIFT or PRESS_RIGHT_SHIFT)
+
+        # determine action based on mouse button
+        if self.button_right:
+            if mod_shift:
+                action = mj.mjtMouse.mjMOUSE_MOVE_H
+            else:
+                action = mj.mjtMouse.mjMOUSE_MOVE_V
+        elif self.button_left:
+            if mod_shift:
+                action = mj.mjtMouse.mjMOUSE_ROTATE_H
+            else:
+                action = mj.mjtMouse.mjMOUSE_ROTATE_V
+        else:
+            action = mj.mjtMouse.mjMOUSE_ZOOM
+
+        mj.mjv_moveCamera(self.model, action, dx/height,
+                          dy/height, self.scene, self.cam)
+
+    def scroll(self, window, xoffset, yoffset):
+        action = mj.mjtMouse.mjMOUSE_ZOOM
+        mj.mjv_moveCamera(self.model, action, 0.0, -0.05 *
+                          yoffset, self.scene, self.cam)
+
+    def simulate(self):
+        while not glfw.window_should_close(self.window):
+            simstart = self.data.time
+
+            while (self.data.time - simstart < 1.0/60.0):
+                mj.mj_step(self.model, self.data)
+
+            # get framebuffer viewport
+            viewport_width, viewport_height = glfw.get_framebuffer_size(
+                self.window)
+            viewport = mj.MjrRect(0, 0, viewport_width, viewport_height)
+
+            # Update scene and render
+            mj.mjv_updateScene(self.model, self.data, self.opt, None, self.cam,
+                               mj.mjtCatBit.mjCAT_ALL.value, self.scene)
+            mj.mjr_render(viewport, self.scene, self.context)
+
+            # swap OpenGL buffers (blocking call due to v-sync)
+            glfw.swap_buffers(self.window)
+
+            # process pending GUI events, call GLFW callbacks
+            glfw.poll_events()
+
+        glfw.terminate()
+
+    def reset(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def controller(self, *args, **kwargs):
+        raise NotImplementedError
