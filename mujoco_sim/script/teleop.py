0fc74f5dcbfbb09c0861ebd3742a3aca6df9955e
jlxue <xuejl2001@mail.ustc.edu.cn>
2024-02-14 19:22:32 +0800

    调参

--- mujoco_sim/script/teleop.py
+++ mujoco_sim/script/teleop.py
@@ -19,9 +19,9 @@ class KeyboardController:
             else:
                 # 根据按键设置线性速度和角速度
                 if key.char == 'w':
-                    self.twist_msg.linear.x = 0.35
+                    self.twist_msg.linear.x = 0.4
                 elif key.char == 's':
-                    self.twist_msg.linear.x = -0.35
+                    self.twist_msg.linear.x = -0.4
                 else:
                     self.twist_msg.linear.x = 0.0
 
